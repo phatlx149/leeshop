e3df60dbf9ecc7f9bdfdda54913ef4969de357ce
username20210914 <phatle1913@gmail.com>
2021-10-05 12:58:44 +0700

    leeshop

---
+++ dao/binhluan.php
@@ -0,0 +1,29 @@
+<?php
+require_once "pdo.php";
+function binhluan_insert($tenHangHoa)
+{
+}
+
+function binhluan_update($idHangHoa, $tenHangHoa)
+{
+}
+
+function binhluan_delete($idHangHoa)
+{
+}
+
+function binhluan_selectall()
+{
+}
+
+function binhluan_select_byid($idHangHoa)
+{
+}
+
+function binhluan_exist($idHangHoa)
+{
+}
+
+function binhluan_select_by_hanghoa()
+{
+}
